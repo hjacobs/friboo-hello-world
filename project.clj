eee6d038560f2374af7f3e6cbead0033543453a2
Henning Jacobs <henning@jacobs1.de>
2015-03-24 17:15:51 +0100

    initial working hello world service

---
+++ project.clj
@@ -0,0 +1,43 @@
+(defproject friboo-hello-world "0.1.0-SNAPSHOT"
+  :description "Hello world example app."
+  :url "https://github.com/hjacobs/friboo-hello-world"
+
+  :license {:name "The Apache License, Version 2.0"
+            :url  "http://www.apache.org/licenses/LICENSE-2.0"}
+
+  :min-lein-version "2.0.0"
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.zalando.stups/friboo "0.4.0"]]
+
+  :main ^:skip-aot helloworld.core
+  :uberjar-name "friboo-hello-world.jar"
+
+  :plugins [[io.sarnowski/lein-docker "1.0.0"]]
+
+  :docker {:image-name "helloworld"}
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["clean"]
+                  ["uberjar"]
+                  ["docker" "build"]
+                  ["docker" "push"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
+
+  :pom-addition [:developers
+                 [:developer {:id "hjacobs"}
+                  [:name "Henning Jacobs"]
+                  [:email "henning@jacobs1.de"]
+                  [:role "Maintainer"]]]
+
+  :profiles {:uberjar {:aot :all}
+
+             :dev     {:repl-options {:init-ns user}
+                       :source-paths ["dev"]
+                       :dependencies [[org.clojure/tools.namespace "0.2.10"]
+                                      [org.clojure/java.classpath "0.2.2"]]}})
---
+++ src/helloworld/api.clj
@@ -0,0 +1,22 @@
+(ns helloworld.api
+  (:require [org.zalando.stups.friboo.system.http :refer [def-http-component]]
+            [ring.util.response :refer :all]
+            [org.zalando.stups.friboo.ring :refer :all]
+            [clojure.tools.logging :as log]))
+
+; define the API component and its dependencies
+(def-http-component API "api.yaml" [db])
+
+(def default-http-configuration
+  {:http-port 8080})
+
+(defn index [request _ _]
+  (-> "Index"
+      (response)
+      (content-type-json)))
+
+(defn say-hello [{:keys [who]} _ _]
+  (log/debug "Saying hello")
+  (-> (str "Hello " who)
+      (response)
+      (content-type-json)))
